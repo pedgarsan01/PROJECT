d593996cef98001e66b1a11af05ddb3a2edafd8d
pedgarsan01 <117484200+pedgarsan01@users.noreply.github.com>
2022-12-09 20:53:51 -0500

    version 20:53

--- client/src/app/planrepas/planrepasForm.ts
+++ client/src/app/planrepas/planrepasForm.ts
@@ -1,4 +1,10 @@
 import { Component} from "@angular/core";
+// import {  ElementRef, ViewChild} from "@angular/core";
+// import { Planrepas } from "../../../../common/tables/Planrepas";
+
+
+// import { CommunicationService } from "./../services/communication.service";
+
 
 @Component({
   selector: "app-planrepas",
@@ -7,6 +13,44 @@ import { Component} from "@angular/core";
 })
 
 export class PlanrepasForm {
-    
+  // @ViewChild("newNumeroplan") newNumeroplan: ElementRef;
+  // @ViewChild("newCategorie") newCategorie: ElementRef;
+  // @ViewChild("newFrequence") newFrequence: ElementRef;
+  // @ViewChild("newNbpersonnes") newNbpersonnes: ElementRef;
+  // @ViewChild("newNbcalories") newNbcalories: ElementRef;
+  // @ViewChild("newPrix") newPrix: ElementRef;
+  // @ViewChild("newNumerofournisseur") newNumerofournisseur: ElementRef;
+
+
+
+  // public planrepass: Planrepas[] = [];
+  // public duplicateError: boolean = false;
+
+  // public constructor(private communicationService: CommunicationService) {}
+
+
+  // public insertPlanrepas(): void {
+  //   const planrepas: Planrepas= {
+  //     numeroplan: this.newNumeroplan.nativeElement.innerText,
+  //     categorie: this.newCategorie.nativeElement.innerText,
+  //     frequence: this.newFrequence.nativeElement.innerText,
+  //     nbpersonnes: this.newNbpersonnes.nativeElement.innerText,
+  //     nbcalories: this.newNbcalories.nativeElement.innerText,
+  //     prix: this.newPrix.nativeElement.innerText,
+  //     numerofournisseur: this.newNumerofournisseur.nativeElement.innerText,
+  //   };
+  //   console.log(planrepas)
+
+  //   this.communicationService.insertPlanrepas(planrepas).subscribe((res: number) => {
+  //     if (res > 0) {
+  //       this.communicationService.filter("update");
+  //     }
+  //     // this.refresh();
+  //     this.duplicateError = res === -1;
+  //   });
+  // }
+
+
+
 }
 
---
+++ server/app/tables/PlanrepasForm.ts
@@ -0,0 +1,9 @@
+export interface PlanrepasForm {
+    numeroplan: string;
+    categorie: string;
+    frequence: string;
+    nbpersonnes: string;
+    nbcalories: string;
+    prix: string;
+    numerofournisseur: string;
+}
